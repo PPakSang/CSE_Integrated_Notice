d0101b0abb16ab68dbc38fcbd0666b002d5825e0
PPakSang <ppm5377@naver.com>
2021-04-21 09:30:52 +0900

    admin site check

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,20 +4,34 @@ import uuid
 
 
 class Genre(models.Model):
+    
+
     name = models.CharField(max_length=200,help_text='Enter a book genre')
 
     def __str__(self):
         return self.name
+
+class Language(models.Model):
+    name=models.CharField(max_length=200,help_text="Enter the book's natural language")
+
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.ForeignKey('Author',on_delete=models.SET_NULL,null=True)
     summary=models.TextField(max_length=1000,help_text='Enter a brief description of the book')
     isbn=models.CharField('ISBN',max_length=13,help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    language=models.ForeignKey('Language',on_delete=models.SET_NULL,null=True)
 
 
     genre=models.ManyToManyField(Genre,help_text='Select a genre for this book') #하나의 책은 여러개의 장르를 가질 수 있다
         
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description='Genre'
+
 
     def __str__(self):
         return self.title
@@ -39,14 +53,16 @@ class BookInstance(models.Model):
         ('a','available'),
         ('r','reserved'),
     )
+    
 
     status=models.CharField(max_length=1,choices=LOAN_STATUS,blank=True,default='m',help_text='Book availability',)
     
     class Meta:
         ordering=['due_back']
     def __str__(self):
+        
         return f'{self.id} ({self.book.title})'
-
+        
     
 
 class Author(models.Model):
