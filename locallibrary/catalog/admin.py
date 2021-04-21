7884b10da13d4f82981e821df09adeacaca74a27
PPakSang <ppm5377@naver.com>
2021-04-21 09:30:39 +0900

    admin site check

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from catalog.models import Author,Book,BookInstance,Genre,Language
 # Register your models here.
+
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+
+@admin.register(Author)
+class registerAdmin(admin.ModelAdmin):
+    list_display=('last_name','first_name','date_of_birth','date_of_death')
+
