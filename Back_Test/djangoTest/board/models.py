0294b2f7247cc7e03e30e56ee09e851d07a21f8c
PPakSang <ppm5377@naver.com>
2021-04-30 15:14:16 +0900

    크롤링 데이터 db 저장

--- Back_Test/djangoTest/board/models.py
+++ Back_Test/djangoTest/board/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Board(models.Model):
-    board_name=models.CharField(max_length=10,help_text='Enter your board_name')
+    board_name=models.CharField(max_length=100,help_text='Enter your board_name')
     post=models.TextField(max_length=1000,help_text='Enter your post')
     
     class Meta:
-        ordering=['-board_name']  # DB에 저장되는 순서
+        pass# ordering=['-board_name']  # DB에 저장되는 순서
 
     def __str__(self):
         return self.board_name
