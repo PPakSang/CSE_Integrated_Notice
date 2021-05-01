18d395c5d5162d91d47932b1e4d57c97c1f2473c
PPakSang <ppm5377@naver.com>
2021-05-02 01:29:13 +0900

    recreate gitignore

--- Backend/notice/views.py
+++ Backend/notice/views.py
@@ -9,6 +9,12 @@ class Notice_listview(generic.ListView):
     model=Uni_post
     template_name='notice/notice_list.html'
 
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        
+        context = super().get_context_data()
+
+        return super().get_context_data(**kwargs)
+
 
 
     
