c66381194dccb29678dd3d53b47d3ae7ffe2f569
PPakSang <ppm5377@naver.com>
2021-05-02 16:24:12 +0900

    bulk_create

--- Backend/notice/views.py
+++ Backend/notice/views.py
@@ -1,7 +1,11 @@
+from os import name
 from django.db import models
 from django.shortcuts import render
 from django.views import generic
 from .models import Tag,Uni_post
+
+
+
 
 # Create your views here.
 
@@ -9,11 +13,19 @@ class Notice_listview(generic.ListView):
     model=Uni_post
     template_name='notice/notice_list.html'
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        
-        context = super().get_context_data()
 
-        return super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        for i in range(1,4):
+            context['tag'+str(i)]=Uni_post.objects.filter(tag__name='멘토링'+str(i))
+
+        return context
+    
+
+        
+
+        
 
 
 
