6c130518f6f8360e456ae3e7431e852dddfc6cd7
hw0603 <hw0603@naver.com>
2021-06-03 01:45:22 +0900

    Update models.py
    attachment_info 필드 최대 글자수 1000자로 확장

--- Backend/notice/models.py
+++ Backend/notice/models.py
@@ -18,7 +18,7 @@ class Uni_post(models.Model):
     post_author = models.CharField(max_length=20, help_text="공지사항 작성자")
     post_date = models.CharField(max_length=50, help_text="공지사항 등록일")
     post_url = models.URLField(max_length=200)
-    attachment_info = models.CharField(max_length=100, help_text='첨부파일 정보', null=True)
+    attachment_info = models.CharField(max_length=1000, help_text='첨부파일 정보', null=True)
     tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
